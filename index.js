f9e679871646a902c0d69f7bc57b59fae970fd1e
an-lee <an.lee.work@gmail.com>
2023-06-24 11:15:37 +0800

    fix mine api

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ module.exports = function () {
 
         var media = {
           autoplay: opts.autoPlay !== false,
-          contentType: opts.type || mime.lookup(url, 'video/mp4'),
+          contentType: opts.type || mime.getType(url, 'video/mp4'),
           metadata: opts.metadata || {
             title: opts.title || '',
             type: 'video', // can be 'video', 'audio' or 'image'
